8a4c3d2b67278c2c97cbf9900af316692289064d
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2025-03-01 20:23:30 +0330

    map template and url added

--- locations/urls.py
+++ locations/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LocationView
-
+from .views import LocationView , MapView 
+app_name = 'locations'
 router = DefaultRouter()
 router.register(r'locations', LocationView)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('map/',MapView.as_view(),name='map')
+    #path('map/', map_view, name='map'),
 ]
--- locations/views.py
+++ locations/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Location
 from .serializers import LocationSerializer
+from django.views.generic.base import TemplateView
 
 class LocationView(viewsets.ModelViewSet):
     queryset =Location.objects.all()
     serializer_class = LocationSerializer
-    
+
+class MapView(TemplateView):
+    template_name = "locations\map.html"
+
+# def map_view(request):
+#      return render(request, 'locations\map.html')
