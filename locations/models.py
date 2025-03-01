cd6bba19260c1be4205b74bd2caa80286f6d0460
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2025-03-01 19:48:12 +0330

    postger database added

---
+++ locations/models.py
@@ -0,0 +1,9 @@
+from django.contrib.gis.db import models
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    coordinates = models.PointField(srid=4326)  # WGS 84 (Latitude/Longitude)
+
+    def __str__(self):
+        return self.name
